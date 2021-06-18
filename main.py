cbbad65baed5bf4514f2ecafb99277e3885a6ebe
brunocercan <bruno.cercan@gmail.com>
2021-06-18 13:36:07 -0300

    adicionando host 0.0.0.0

--- main.py
+++ main.py
@@ -96,4 +96,4 @@ def converte_cliente(clientes):
         return a_dict #ordenando as tuplas em id, nome, email, cpf e sexo   
     return jsons.dump(list(map(cria_cliente_com_tupla, clientes)))
 
-app.run()
+app.run(host='0.0.0.0')
